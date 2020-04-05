a032eacd6c9646cf8247711a498f7af31a78b893
Peter Binkley <peter.binkley@ualberta.ca>
2020-04-05 11:44:34 -0600

    Use Github token to push

--- github.sh
+++ github.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+source ./github-credentials
+
 PROJECTDIR=$1
-DATE=`date "+%Y-%m-%dT%H:%M:%S"`
+source ./projects/$PROJECTDIR/github-repo
+
+DATE=`date "+%Y-%m-%dT%H-%M-%S"`
 
 cd projects/$PROJECTDIR/html
 git add .
 git commit -m "$DATE update"
-git push
-print "Pushed commit: $DATE"
+git push https://$GITHUB_TOKEN@github.com/pbinkley/$GITHUB_REPO.git
+echo "Pushed commit: $DATE"
